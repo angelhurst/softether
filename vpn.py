3f5d471c173165e5824bafc02c73d09e97150c1a
angelhurst <48533269+angelhurst@users.noreply.github.com>
2023-05-01 09:32:51 -0400

    Update vpn.py

--- vpn.py
+++ vpn.py
@@ -2,39 +2,27 @@ import subprocess
 from os import system
 import json
 
-consulta  = "/usr/local/vpnserver/vpncmd /SERVER localhost:5555 /PASSWORD:holara2023 /HUB:myhub /CMD iptable"
-table_ip = subprocess.check_output(consulta, shell=True)
-table_ip = table_ip.decode('utf-8')
 
-# table_ip = """IpTable command - Get the IP Address Table Database
-# Item        |Value
-# ------------+--------------------
-# ID          |3245080289
-# Session Name|SID-SECURENAT-1
-# IP Address  |192.168.30.1
-# Created at  |2023-05-01 02:33:46
-# Updated at  |2023-05-01 06:08:53
-# Location    |On 'ip-172-26-10-68'
-# ------------+--------------------
-# ID          |28800360
-# Session Name|SID-VPNUSER-[L2TP]-7
-# IP Address  |192.168.30.10 (DHCP)
-# Created at  |2023-05-01 05:40:23
-# Updated at  |2023-05-01 06:08:55
-# Location    |On 'ip-172-26-10-68'
-# The command completed successfully."""
+def parsing(commad):
 
-resultado = system(consulta)
-item = table_ip.split('\n------------+--------------------\n')
+    table = subprocess.check_output(commad, shell=True)
+    table = table.decode('utf-8')
 
-data = []
-for index_item in range(1,len(item)):
-    session_keys = {}
-    for index_key in item[index_item].split('\n'):
+    item = table.split('\n------------+--------------------\n')
 
-        if index_key.find('|') != -1:
-            session_keys[index_key.split('|')[0].strip()]= index_key.split('|')[1].strip()
-    
-    data.append(session_keys)
+    data = []
+    for index_item in range(1,len(item)):
+        session_keys = {}
+        for index_key in item[index_item].split('\n'):
 
-print(json.dumps(data, indent=4))
+            if index_key.find('|') != -1:
+                session_keys[index_key.split('|')[0].strip()]= index_key.split('|')[1].strip()
+        
+        data.append(session_keys)
+    return(data)
+
+table_ip = parsing("/usr/local/vpnserver/vpncmd /SERVER localhost:5555 /PASSWORD:holara2023 /HUB:myhub /CMD iptable")
+table_dhcp = parsing("/usr/local/vpnserver/vpncmd /SERVER localhost:5555 /PASSWORD:holara2023 /HUB:myhub /CMD dhcptable")
+
+print(json.dumps(table_ip, indent=4))
+print(json.dumps(table_dhcp, indent=4))
