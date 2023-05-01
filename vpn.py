2b02a3cc76f2acd833a66a35972b986717017e75
angelhurst <48533269+angelhurst@users.noreply.github.com>
2023-05-01 09:58:27 -0400

    Update vpn.py

--- vpn.py
+++ vpn.py
@@ -9,7 +9,31 @@ def parsing(commad):
     table = table.decode('utf-8')
     print(table)
 
-    item = table.split('\n------------+--------------------\n')
+#     table="""IpTable command - Get the IP Address Table Database
+# Item        |Value
+# ------------+--------------------0
+# ID          |3245080289
+# Session Name|SID-SECURENAT-1
+# IP Address  |192.168.30.1
+# Created at  |2023-05-01 02:33:46
+# Updated at  |2023-05-01 13:42:34
+# Location    |On 'ip-172-26-10-68'
+# ------------+--------------------0
+# ID          |2294021727
+# Session Name|SID-VPNUSER-[L2TP]-8
+# IP Address  |192.168.30.10 (DHCP)
+# Created at  |2023-05-01 13:30:46
+# Updated at  |2023-05-01 13:42:34
+# Location    |On 'ip-172-26-10-68'
+# The command completed successfully."""
+
+    separtor = ""
+    for row in table.split('\n'):
+        if row.find('-+-') != -1:
+            separtor = row
+
+
+    item = table.split(separtor)
 
     data = []
     for index_item in range(1,len(item)):
