bcad6d2d00135821a36aa6c8b5fa02bf5b49e4a2
angelhurst <48533269+angelhurst@users.noreply.github.com>
2023-05-02 20:19:32 -0400

    Update views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,32 +3,20 @@ from django.views import View
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import authentication, permissions
+from django.conf import settings
 import subprocess
 import json
 
 def parsing(commad,):
+    """recupera datos del vpn y los convierte a un dict
+
+    Args:
+        commad (string): comando que se ejecutara
+    """
 
     table = subprocess.check_output(commad, shell=True)
     table = table.decode('utf-8')
 
-#     table="""IpTable command - Get the IP Address Table Database
-# Item        |Value
-# ------------+--------------------0
-# ID          |3245080289
-# Session Name|SID-SECURENAT-1
-# IP Address  |192.168.30.1
-# Created at  |2023-05-01 02:33:46
-# Updated at  |2023-05-01 13:42:34
-# Location    |On 'ip-172-26-10-68'
-# ------------+--------------------0
-# ID          |2294021727
-# Session Name|SID-VPNUSER-[L2TP]-19
-# IP Address  |192.168.30.10 (DHCP)
-# Created at  |2023-05-01 13:30:46
-# Updated at  |2023-05-01 13:42:34
-# Location    |On 'ip-172-26-10-68'
-# The command completed successfully."""
-
     separtor = ""
     for row in table.split('\n'):
         if row.find('-+-') != -1:
@@ -46,49 +34,17 @@ def parsing(commad,):
                 session_keys[index_key.split('|')[0].strip()]= index_key.split('|')[1].strip()
         
         data.append(session_keys)
-    return(data)
 
-def parsing2(commad,):
-
-    # table = subprocess.check_output(commad, shell=True)
-    # table = table.decode('utf-8')
-
-    table="""DhcpTable command - Get Virtual DHCP Server Function Lease Table of SecureNAT Function
-Item            |Value
-----------------+-------------------------
-ID              |19
-Leased at       |2023-05-01 (Mon) 14:10:47
-Expires at      |2023-05-01 (Mon) 16:10:47
-MAC Address     |CA-81-1B-57-E4-C2
-Allocated IP    |192.168.30.10
-Client Host Name|MacBook-Air-de-jose.local
-The command completed successfully."""
-
-    separtor = ""
-    for row in table.split('\n'):
-        if row.find('-+-') != -1:
-            separtor = row
-
-
-    item = table.split(separtor)
-
-    data = []
-    for index_item in range(1,len(item)):
-        session_keys = {}
-        for index_key in item[index_item].split('\n'):
-
-            if index_key.find('|') != -1:
-                session_keys[index_key.split('|')[0].strip()]= index_key.split('|')[1].strip()
-        
-        data.append(session_keys)
     return(data)
 
 class ListDevice(APIView):
+    """Api de Dispotivos conectados
+    """
 
     def get(self, request, format=None):
 
-        table_ip = parsing('/usr/local/vpnserver/vpncmd /SERVER localhost:5555 /PASSWORD:holara2023 /HUB:myhub /CMD iptable')
-        table_dhcp = parsing('/usr/local/vpnserver/vpncmd /SERVER localhost:5555 /PASSWORD:holara2023 /HUB:myhub /CMD dhcptable')
+        table_ip = parsing('/usr/local/vpnserver/vpncmd /SERVER localhost:5555 /PASSWORD:{} /HUB:{}} /CMD iptable'.format(settings.VPN_PASS,settings.HUB))
+        table_dhcp = parsing('/usr/local/vpnserver/vpncmd /SERVER localhost:5555 /PASSWORD:{} /HUB:{}} /CMD dhcptable'.format(settings.VPN_PASS,settings.HUB))
 
         device=[]
         for dhcp in table_dhcp:
